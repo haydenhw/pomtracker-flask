a8d4886489e278db5c1a35e4478ca2fb019791e2
hayden7913 <hayden321@gmail.com>
2020-10-02 22:33:29 -0700

    minor readability tweaks

--- pomtracker/__init__.py
+++ pomtracker/__init__.py
@@ -8,6 +8,7 @@ from pomtracker.extensions import db
 from pomtracker.ping import ping_blueprint
 
 def create_app():
+
     # instantiate the app
     app = Flask(__name__)
     CORS(app)
@@ -18,11 +19,11 @@ def create_app():
     db.init_app(app)
 
     # register views
-    app.register_blueprint(ping_blueprint)
     app.add_url_rule('/api/projects', view_func=ProjectList.as_view('project_list'))
     app.add_url_rule('/api/projects/<int:project_id>', view_func=Project.as_view('project'))
     app.add_url_rule('/api/tasks', view_func=TaskList.as_view('task_list'))
     app.add_url_rule('/api/tasks/<int:task_id>', view_func=Task.as_view('task'))
+    app.register_blueprint(ping_blueprint)
 
     # register error handling
     @app.errorhandler(ValidationError)
